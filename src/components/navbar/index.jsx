2d6049134e2064bcd8184921e72d027e8afce50a
陈钟杰 <chenzhongjie@yetingfm.com>
2020-03-20 17:50:13 +0800

    优化navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,98 +1,29 @@
 import Taro, { useState, useDidShow } from '@tarojs/taro';
+import { View, Text } from '@tarojs/components';
 import _isFunction from 'lodash/isFunction';
-import { View } from '@tarojs/components';
+import { getGlobalData, setGlobalData } from '@/utils/global_data';
+
 import './index.less';
 
-function getSystemInfo() {
-  if (Taro.globalSystemInfo && !Taro.globalSystemInfo.ios) {
-    return Taro.globalSystemInfo;
-  } else {
-    // h5环境下忽略navbar
-    if (!_isFunction(Taro.getSystemInfoSync)) {
-      return null;
-    }
-    let systemInfo = Taro.getSystemInfoSync() || {
-      model: '',
-      system: ''
-    };
-    let ios = !!(systemInfo.system.toLowerCase().search('ios') + 1);
-    let rect;
-    try {
-      rect = Taro.getMenuButtonBoundingClientRect ? Taro.getMenuButtonBoundingClientRect() : null;
-      if (rect === null) {
-        throw 'getMenuButtonBoundingClientRect error';
-      }
-      //取值为0的情况  有可能width不为0 top为0的情况
-      if (!rect.width || !rect.top || !rect.left || !rect.height) {
-        throw 'getMenuButtonBoundingClientRect error';
-      }
-    } catch (error) {
-      let gap = ''; //胶囊按钮上下间距 使导航内容居中
-      let width = 96; //胶囊的宽度
-      if (systemInfo.platform === 'android') {
-        gap = 8;
-        width = 96;
-      } else if (systemInfo.platform === 'devtools') {
-        if (ios) {
-          gap = 5.5; //开发工具中ios手机
-        } else {
-          gap = 7.5; //开发工具中android和其他手机
-        }
-      } else {
-        gap = 4;
-        width = 88;
-      }
-      if (!systemInfo.statusBarHeight) {
-        //开启wifi的情况下修复statusBarHeight值获取不到
-        systemInfo.statusBarHeight = systemInfo.screenHeight - systemInfo.windowHeight - 20;
-      }
-      rect = {
-        //获取不到胶囊信息就自定义重置一个
-        bottom: systemInfo.statusBarHeight + gap + 32,
-        height: 32,
-        left: systemInfo.windowWidth - width - 10,
-        right: systemInfo.windowWidth - 10,
-        top: systemInfo.statusBarHeight + gap,
-        width: width
-      };
-      console.log('error', error);
-      console.log('rect', rect);
-    }
+function Index(props) {
 
-    let navBarHeight = '';
-    if (!systemInfo.statusBarHeight) {
-      //开启wifi和打电话下
-      systemInfo.statusBarHeight = systemInfo.screenHeight - systemInfo.windowHeight - 20;
-      navBarHeight = (function() {
-        let gap = rect.top - systemInfo.statusBarHeight;
-        return 2 * gap + rect.height;
-      })();
+  const {
+    className = '',
+    background = 'rgba(255, 255, 255, 1)',
+    placeholder = true, // 是否需要导航栏占位
+    // backgroundColorTop = 'rgba(255, 255, 255, 1)',
+    color = 'rgba(0, 0, 0, 1)',
+    title = '',
+    back = false,
+    home = false,
+    delta = 1, // 返回的页面数，如果 delta 大于现有页面数，则返回到首页。
+    iconTheme = 'black', // icon的主体色；默认：'black'（黑色）；'white'（白色）
+    onBack,
+    onHome
+  } = props;
 
-      systemInfo.statusBarHeight = 0;
-      systemInfo.navBarExtendHeight = 0; //下方扩展4像素高度 防止下方边距太小
-    } else {
-      navBarHeight = (function() {
-        let gap = rect.top - systemInfo.statusBarHeight;
-        return systemInfo.statusBarHeight + 2 * gap + rect.height;
-      })();
-      if (ios) {
-        systemInfo.navBarExtendHeight = 4; //下方扩展4像素高度 防止下方边距太小
-      } else {
-        systemInfo.navBarExtendHeight = 0;
-      }
-    }
+  let globalSystemInfo = getSystemInfo();
 
-    systemInfo.navBarHeight = navBarHeight; //导航栏高度不包括statusBarHeight
-    systemInfo.capsulePosition = rect; //右上角胶囊按钮信息bottom: 58 height: 32 left: 317 right: 404 top: 26 width: 87 目前发现在大多机型都是固定值 为防止不一样所以会使用动态值来计算nav元素大小
-    systemInfo.ios = ios; //是否ios
-    Taro.globalSystemInfo = systemInfo; //将信息保存到全局变量中,后边再用就不用重新异步获取了
-    //console.log('systemInfo', systemInfo);
-    return systemInfo;
-  }
-}
-let globalSystemInfo = getSystemInfo();
-
-function Navbar(props) {
   const [configStyle, setConfigStyle] = useState(setStyle(globalSystemInfo));
   const {
     navigationbarinnerStyle,
@@ -104,61 +35,48 @@ function Navbar(props) {
     rightDistance
   } = configStyle;
 
-  const {
-    placeholder = true, // 是否需要占位
-    extClass = '',
-    background = 'rgba(255,255,255,1)', //导航栏背景
-    color = '#000000',
-    title = '',
-    backgroundColorTop,
-    searchText = '点我搜索',
-    searchBar = false,
-    back = false,
-    home = false,
-    iconTheme = 'black',
-    delta = 1,
-    onHome,
-    onSearch,
-    onBack
-  } = props;
-
   useDidShow(() => {
     if (globalSystemInfo.ios) {
       globalSystemInfo = getSystemInfo();
       setConfigStyle(setStyle(globalSystemInfo));
     }
-  });
+  })
 
   function setStyle(systemInfo) {
-    const { statusBarHeight, navBarHeight, capsulePosition, navBarExtendHeight, ios, windowWidth } = systemInfo;
-    let rightDistance = windowWidth - capsulePosition.right; //胶囊按钮右侧到屏幕右侧的边距
-    let leftWidth = windowWidth - capsulePosition.left; //胶囊按钮左侧到屏幕右侧的边距
+    const {
+      statusBarHeight,
+      navBarHeight,
+      capsulePosition,
+      navBarExtendHeight,
+      ios,
+      windowWidth
+    } = systemInfo;
 
-    let navigationbarinnerStyle = [
-      `color:${color}`,
-      //`background:${background}`,
+    const rightDistance = windowWidth - capsulePosition.right; // 胶囊按钮右侧到屏幕右侧的边距
+    const leftWidth = windowWidth - capsulePosition.left; // 胶囊按钮左侧到屏幕右侧的边距
+
+    const navigationbarinnerStyle = [
+      `color: ${color}`,
+      `background: ${background}`,
       `height:${navBarHeight + navBarExtendHeight}px`,
       `padding-top:${statusBarHeight}px`,
       `padding-right:${leftWidth}px`,
       `padding-bottom:${navBarExtendHeight}px`
     ].join(';');
+
     let navBarLeft = [];
     if ((back && !home) || (!back && home)) {
-      navBarLeft = [
-        `width:${capsulePosition.width}px`,
-        `height:${capsulePosition.height}px`,
-        `margin-left:0px`,
-        `margin-right:${rightDistance}px`
-      ].join(';');
+      navBarLeft = [`width:${capsulePosition.width}px`, `height:${capsulePosition.height}px`].join(';')
     } else if ((back && home) || title) {
       navBarLeft = [
         `width:${capsulePosition.width}px`,
         `height:${capsulePosition.height}px`,
         `margin-left:${rightDistance}px`
-      ].join(';');
+      ].join(';')
     } else {
-      navBarLeft = [`width:auto`, `margin-left:0px`].join(';');
+      navBarLeft = ['width:auto', 'margin-left:0px'].join(';')
     }
+
     return {
       navigationbarinnerStyle,
       navBarLeft,
@@ -168,9 +86,109 @@ function Navbar(props) {
       ios,
       rightDistance
     };
-  };
+  }
 
-  const handleBackClick = () => {
+  // 获取用户手机基本信息
+  function getSystemInfo() {
+    let systemInfo = getGlobalData('globalSystemInfo');
+    if (systemInfo) {
+      return systemInfo;
+    } else {
+      systemInfo = Taro.getSystemInfoSync();
+      const ios = !!(systemInfo.system.toLowerCase().search('ios') + 1);
+      systemInfo.ios = ios
+      const rect = getMenuButtonBoundingClientRect(systemInfo);
+
+      let navBarHeight;
+      if (!systemInfo.statusBarHeight) {
+        systemInfo.statusBarHeight = systemInfo.screenHeight - systemInfo.windowHeight - 20;
+
+        navBarHeight = (function () {
+          const gap = rect.top - systemInfo.statusBarHeight;
+          return 2 * gap + rect.height;
+        }());
+
+        systemInfo.statusBarHeight = 0;
+        systemInfo.navBarExtendHeight = 0; // 下方扩展4像素高度 防止下方边距太小
+      } else {
+        navBarHeight = (function () {
+          const gap = rect.top - systemInfo.statusBarHeight;
+          return systemInfo.statusBarHeight + 2 * gap + rect.height;
+        }());
+
+        if (ios) {
+          systemInfo.navBarExtendHeight = 4; // 下方扩展4像素高度 防止下方边距太小
+        } else {
+          systemInfo.navBarExtendHeight = 0;
+        }
+      }
+
+      systemInfo.navBarHeight = navBarHeight; // 导航栏高度不包括statusBarHeight
+      systemInfo.capsulePosition = rect;
+      systemInfo.ios = ios; // 是否ios
+
+      setGlobalData('globalSystemInfo', systemInfo); // 将信息保存到全局变量中,后边再用就不用重新异步获取了
+      // console.log('systemInfo', systemInfo);
+      globalSystemInfo = systemInfo;
+      return systemInfo;
+    }
+  }
+
+  // 检查获取到的元素信息
+  function checkRect(rect) {
+    // 胶囊信息4种任一属性为0返回true
+    return !rect.width || !rect.top || !rect.left || !rect.height;
+  }
+
+  // 获取菜单按钮（右上角胶囊按钮）的布局位置信息
+  function getMenuButtonBoundingClientRect(systemInfo) {
+    const { ios } = systemInfo;
+    // 胶囊高度；ios和Android一致
+    const capsuleHeight = 32;
+    let rect;
+    try {
+      rect = Taro.getMenuButtonBoundingClientRect ? Taro.getMenuButtonBoundingClientRect() : null;
+      if (rect === null) {
+        throw new Error('getMenuButtonBoundingClientRect Error');
+      }
+      // 取值为0的情况  有可能width不为0 top为0的情况
+      if (checkRect(rect)) {
+        throw new Error('getMenuButtonBoundingClientRect Error');
+      }
+    } catch (error) {
+      let gap = 4; // 胶囊按钮上下间距；使导航内容居中
+      let width = 88; // 胶囊宽度
+      if (systemInfo.platform === 'android') {
+        // 安卓机
+        gap = 8;
+        width = 96;
+      } else if (systemInfo.platform === 'devtools') {
+        // 开发工具的模拟器
+        if (ios) {
+          gap = 5.5;
+        } else {
+          gap = 7.5;
+        }
+      }
+      if (!systemInfo.statusBarHeight) {
+        // 开启wifi的情况下修复statusBarHeight值获取不到
+        systemInfo.statusBarHeight = systemInfo.screenHeight - systemInfo.windowHeight - 20;
+      }
+      // 获取不到胶囊信息就自定义重置一个
+      rect = {
+        top: systemInfo.statusBarHeight + gap,
+        bottom: systemInfo.statusBarHeight + gap + capsuleHeight,
+        left: systemInfo.windowWidth - width - 10,
+        right: systemInfo.windowWidth - 10,
+        height: capsuleHeight,
+        width
+      };
+    }
+
+    return rect;
+  }
+
+  function handleBackClick() {
     if (_isFunction(onBack)) {
       onBack();
     } else {
@@ -183,19 +201,9 @@ function Navbar(props) {
     }
   }
 
-  const handleGoHomeClick = () => {
+  function handleGoHomeClick() {
     if (_isFunction(onHome)) {
       onHome();
-    } else {
-      Taro.reLaunch({
-        url: '/pages/home/index'
-      });
-    }
-  }
-
-  const handleSearchClick = () => {
-    if (_isFunction(onSearch)) {
-      onSearch();
     }
   }
 
@@ -203,73 +211,70 @@ function Navbar(props) {
   let need_bar__center = true;
 
   if (title) {
-    nav_bar__center = <text className="title">{title}</text>;
-  } else if (searchBar) {
-    nav_bar__center = (
-      <View
-        className='navbar-container-search'
-        style={`height:${capsulePosition.height}px;`}
-        onClick={handleSearchClick.bind(this)}
-      >
-        <View className='navbar-container-search__icon' />
-        <View className='navbar-container-search__input'>{searchText}</View>
-      </View>
-    );
+    nav_bar__center = <Text className='title'>{title}</Text>;
   } else {
+    // nav_bar__center = props.renderCenter; // 无效，taro针对render props的bug
     need_bar__center = false;
-    // nav_bar__center = props.renderCenter;
   }
 
   return (
-    <View
-      className={`navbar-container ${ios ? 'ios' : 'android'} ${extClass}`}
-      style={`background: ${backgroundColorTop ? backgroundColorTop : background};height:${placeholder ? navBarHeight +
-        navBarExtendHeight : 0}px;`}
-    >
       <View
-        className={`navbar-container__placeholder ${ios ? 'ios' : 'android'}`}
-        style={`padding-top: ${navBarHeight + navBarExtendHeight}px;`}
-      />
-      <View
-        className={`navbar-container__inner ${ios ? 'ios' : 'android'}`}
-        style={`background:${background};${navigationbarinnerStyle};`}
+        className={`nav-bar-component ${ios ? 'ios' : 'android'} ${className}`}
       >
-        <View className='navbar-container__left' style={navBarLeft}>
-          {back && !home && (
-            <View
-              onClick={handleBackClick.bind(this)}
-              className={`navbar-container__button navbar-container__btn_goback ${iconTheme}`}
-            />
-          )}
-          {!back && home && (
-            <View
-              onClick={handleGoHomeClick.bind(this)}
-              className={`navbar-container__button navbar-container__btn_gohome ${iconTheme}`}
-            />
-          )}
-          {back && home && (
-            <View className={`navbar-container__buttons ${ios ? 'ios' : 'android'}`}>
+        {
+          // 导航栏占位
+          placeholder
+          &&
+          <View
+            className={`nav-bar__placeholder ${ios ? 'ios' : 'android'}`}
+            style={`padding-top: ${navBarHeight + navBarExtendHeight}px;`}
+          />
+        }
+        <View
+          className={`nav-bar__inner ${ios ? 'ios' : 'android'}`}
+          style={`background:${background};${navigationbarinnerStyle};`}
+        >
+          <View className='nav-bar__left' style={navBarLeft}>
+            {back && !home && (
               <View
                 onClick={handleBackClick.bind(this)}
-                className={`navbar-container__button navbar-container__btn_goback ${iconTheme}`}
+                className={`nav-bar__button nav-bar__btn_goback ${iconTheme}`}
               />
+            )}
+            {!back && home && (
               <View
                 onClick={handleGoHomeClick.bind(this)}
-                className={`navbar-container__button navbar-container__btn_gohome ${iconTheme}}`}
+                className={`nav-bar__button nav-bar__btn_gohome ${iconTheme}`}
               />
-            </View>
-          )}
-          {!back && !home && props.renderLeft}
-        </View>
-        <View className='navbar-container__center' style={`padding-left: ${rightDistance}px`}>
-          {need_bar__center ? nav_bar__center : props.renderCenter}
-        </View>
-        <View className='navbar-container__right' style={`margin-right: ${rightDistance}px`}>
-          {props.renderRight}
+            )}
+            {back && home && (
+              <View className={`nav-bar__buttons ${ios ? 'ios' : 'android'}`}>
+                <View
+                  onClick={handleBackClick.bind(this)}
+                  className={`nav-bar__button nav-bar__btn_goback ${iconTheme}`}
+                />
+                <View
+                  onClick={handleGoHomeClick.bind(this)}
+                  className={`nav-bar__button nav-bar__btn_gohome ${iconTheme}}`}
+                />
+              </View>
+            )}
+            {!back && !home && props.renderLeft}
+          </View>
+          <View className='nav-bar__center' style={`padding-left: ${rightDistance}px`}>
+            {need_bar__center ? nav_bar__center : props.renderCenter}
+          </View>
+          <View className='nav-bar__right' style={`margin-right: ${rightDistance}px`}>
+            {props.renderRight}
+          </View>
         </View>
       </View>
-    </View>
   );
 }
 
-export default Navbar;
+Index.options = {
+  multipleSlots: true,
+  addGlobalClass: true
+}
+
+export default Index;
